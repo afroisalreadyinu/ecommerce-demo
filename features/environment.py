76b749329adf4c81312609f56ffe9b1232bea2b3
Ulas Tuerkmen <utuerkmen@newstore.com>
2016-10-24 22:18:53 +0200

    Use environment to set test DB, tear down

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,17 @@
 import os
 import sys
 import tempfile
-from ecomm_demo import app, create_db
+from ecomm_demo import app
+from ecomm_demo.run import create_db
 
 def before_feature(context, feature):
     app.config['TESTING'] = True
-    context.db, app.config['DATABASE'] = tempfile.mkstemp()
+    context.db, context.db_path = tempfile.mkstemp()
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(context.db_path)
     create_db()
     context.client = app.test_client()
 
 
 def after_feature(context, feature):
     os.close(context.db)
-    os.unlink(app.config['DATABASE'])
+    os.unlink(context.db_path)
