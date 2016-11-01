2e21193e5efb7922007d84a362b71d68e478257c
Ulas Tuerkmen <utuerkmen@newstore.com>
2016-11-01 11:15:22 +0100

    Code for satifying product import feature

--- ecomm_demo/models.py
+++ ecomm_demo/models.py
@@ -8,3 +8,9 @@ class User(db.Model):
     email = db.Column(db.String(), primary_key=True)
     pw_hash = db.Column(db.String(), nullable=False)
     company = db.Column(db.String(), nullable=False)
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(), nullable=False)
+    gtin = db.Column(db.String(14), nullable=False)
--- ecomm_demo/views.py
+++ ecomm_demo/views.py
@@ -3,7 +3,7 @@ from sqlalchemy import exc
 from passlib.apps import custom_app_context
 
 from ecomm_demo import app
-from .models import db, User
+from .models import db, User, Product
 
 @app.route("/")
 def index():
@@ -43,3 +43,19 @@ def login():
         return jsonify({"email":user.email})
     else:
         abort(401)
+
+@app.route("/products", methods=["POST"])
+def add_products():
+    data = request.get_json()
+    for product in data:
+        product = Product(**product)
+        db.session.add(product)
+    db.session.commit()
+    return jsonify({"status": "ok"})
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    def to_dict(p):
+        return {'id': p.id, 'label': p.label, 'gtin': p.gtin}
+    products = Product.query.all()
+    return jsonify([to_dict(p) for p in products])
