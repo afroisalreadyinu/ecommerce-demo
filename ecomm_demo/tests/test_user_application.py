ecb32f04d3b29b4842d94068a947ec4f8739df8d
Ulas Tuerkmen <ulas@goodscloud.com>
2016-11-07 21:16:03 +0100

    Fake objects, test for login

--- ecomm_demo/tests/test_user_application.py
+++ ecomm_demo/tests/test_user_application.py
@@ -10,14 +10,40 @@ VALID_EMAIL = 'goofy@acme.com'
 VALID_PASS = 'testpass'
 VALID_COMPANY = 'Acme Inc'
 
+class ResultSet:
+
+    def __init__(self, result):
+        self.result = result
+
+    def one(self):
+        return self.result[0]
+
 class MockUserTable:
+
+    def __init__(self, existing=None):
+        self.existing = existing or []
 
     def new_row(self, email, pw_hash, company):
         return UserRow(email, pw_hash, company)
 
+    @property
+    def query(self):
+        return self
+
+    def filter_by(self, **filters):
+        return ResultSet(self.existing)
+
 class MockSecurityContext:
+
+    def __init__(self, fail=False):
+        self.fail = fail
+
     def encrypt(self, password):
         return 'encrypted'
+
+    def verify(self, password, hash):
+        return not self.fail
+
 
 class TestuserApplication(unittest.TestCase):
 
@@ -40,3 +66,11 @@ class TestuserApplication(unittest.TestCase):
         app = UserApplication(DuplicateUserMockTable(), MockSecurityContext())
         with self.assertRaises(UserApplicationError):
             app.signup(VALID_EMAIL, VALID_PASS, VALID_COMPANY)
+
+
+    def test_login_no_error(self):
+        existing = [UserRow(VALID_EMAIL, VALID_PASS, VALID_COMPANY)]
+        app = UserApplication(MockUserTable(existing=existing),
+                              MockSecurityContext())
+        user = app.login(VALID_EMAIL, VALID_PASS)
+        self.assertEqual(user.email, VALID_EMAIL)
