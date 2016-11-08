a15f030c40a029dc44366455c66dfce8a37488e3
Ulas Tuerkmen <ulas@goodscloud.com>
2016-11-08 23:17:33 +0100

    Added the company table and relation to user

--- ecomm_demo/models.py
+++ ecomm_demo/models.py
@@ -17,7 +17,21 @@ class User(db.Model, EcommerceModel):
     __tablename__ = 'user'
     email = db.Column(db.String(), primary_key=True)
     pw_hash = db.Column(db.String(), nullable=False)
-    company = db.Column(db.String(), nullable=False)
+    company_id = db.Column(
+        db.Integer,
+        db.ForeignKey('company.id', ondelete='RESTRICT'),
+        nullable=False,
+    )
+    company = db.relationship(
+        'Company',
+        backref=db.backref('members', order_by=[id]),
+        foreign_keys=[company_id],
+    )
+
+class Company(db.Model, EcommerceModel):
+    __tablename__ = 'company'
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(), nullable=False)
 
 
 class Product(db.Model, EcommerceModel):
