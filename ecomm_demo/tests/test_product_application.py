79a45bfe9166540bf51dda96fa487788ffd9dba3
Ulas Tuerkmen <utuerkmen@newstore.com>
2016-11-27 22:55:53 +0100

    Use common mock table in product tests

--- ecomm_demo/tests/test_product_application.py
+++ ecomm_demo/tests/test_product_application.py
@@ -2,24 +2,13 @@ import unittest
 from collections import namedtuple
 
 from ecomm_demo.product_application import ProductApplication
+from common import MockTable
 
 GTIN = '00845982006196'
 ProductRow = namedtuple('UserRow', 'label gtin company')
 
-class MockProductTable:
-
-    def __init__(self, existing=None):
-        self.existing = existing or []
-
-    def new_row(self, commit, **kwargs):
-        return ProductRow(**kwargs)
-
-    @property
-    def query(self):
-        return self
-
-    def all(self):
-        return self.existing
+class MockProductTable(MockTable):
+    ROW_CLASS = ProductRow
 
 class TestProductApplication(unittest.TestCase):
 
@@ -28,7 +17,9 @@ class TestProductApplication(unittest.TestCase):
         app = ProductApplication(product_table)
         product = app.add_product(label='A label', gtin=GTIN, company='puma')
         self.assertEqual(product.label, 'A label')
-        self.assertEqual(product.company, 'puma')
+        self.assertEqual(len(product_table.existing), 1)
+        self.assertEqual(product_table.existing[0].company, 'puma')
+
 
     def test_query_products(self):
         existing = [ProductRow(label='test', gtin=GTIN)]
