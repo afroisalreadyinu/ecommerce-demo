857a20811c1c8feb574b118c1c744b9e66523f1a
Ulas Tuerkmen <ulas.tuerkmen@gmail.com>
2017-10-26 23:37:47 +0200

    Working in using redux

--- ecomm-frontend/src/index.js
+++ ecomm-frontend/src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import PropTypes from 'prop-types'
 import { fromJS } from 'immutable';
 
-const initialState = fromJS({userEmail: null})
+const initialState = fromJS({userEmail: null});
 
-var h1 = React.createElement('h1', {}, 'Hello World');
+// Reducer
+
+function ecommerceReducer(state=initialState, action) {
+  switch(action.type) {
+  case 'LOGIN':
+    return Object.assign({}, state, {email: action.email});
+  default:
+    return initialState;
+  };
+};
+
+// Components
+
+const SetEmail = ({ onSubmit }) => (
+  <form onSubmit={onSubmit} >
+    <input placeholder="Email"></input>
+    <input type="submit" value="Set"></input>
+  </form>
+)
+
+SetEmail.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+}
+
+function setEmail() {
+  console.log("Something happened");
+}
+
+const VisibleSetEmail = connect(
+  state => state.userEmail,
+  mapDispatchToProps
+)(SetEmail)
+
+const App = () => (
+  <div>
+    <SetEmail onSubmit={setEmail}/>
+  </div>
+)
+
+let store = createStore(ecommerceReducer);
+
 ReactDOM.render(
-  h1,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
