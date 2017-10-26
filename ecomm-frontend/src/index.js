8136994c4e3c9050a31359f30699435f9915f709
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2017-10-26 17:47:13 +0200

    Working on introducing redux

--- ecomm-frontend/src/index.js
+++ ecomm-frontend/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { fromJS } from 'immutable';
+
+const initialState = fromJS({userEmail: null})
+
 var h1 = React.createElement('h1', {}, 'Hello World');
 ReactDOM.render(
   h1,
